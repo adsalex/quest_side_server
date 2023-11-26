d0693dc131b3d34473997b7b2d25f7fc6a170529
Alex_s <adsalex77@gmail.com>
2023-11-26 12:54:11 +0300

    small fixes

--- express.js
+++ express.js
@@ -4,25 +4,34 @@ const cors = require("cors")
  const express = require("express")
  const app = express()
  const dir = "./quests"
+console.log("server running. let's quest")
 var filename = filesys.readdirSync(dir,{withFileTypes: true})
 app.use(cors())
 app.use("/list",function (request, response) {
     response.send(filename);
 }); 
 app.use("/preload",function (request, response) {
-    console.log(request.query.q)
+    
+    if(filesys.existsSync(dir+"/"+request.query.q+".json"))
+    {
     let filebuf = filesys.readFileSync(dir+"/"+request.query.q+".json","utf-8")
     response.send(JSON.parse(filebuf).description);
+    }
+    else{response.send(JSON.stringify({error:"404"}))}
+    
 });
 app.use("/load",function (request, response) {
+    if(filesys.existsSync(dir+"/"+request.query.q+".json"))
+    {
     let filebuf = filesys.readFileSync(dir+"/"+request.query.q+".json","utf-8")
     response.send(filebuf);
+    }
+    else
+    {
+        response.send(JSON.stringify({error:"404"}))
+    }
 });  
 app.use(function (request, response) {
-    //console.log(request.query.fisle)
-    //filename
-    //response.sendFile(dir+questname)
     response.send(request.query);
-    //response.send("{Hello:11}");
 });
 app.listen(3300);
